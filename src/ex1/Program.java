530be8283153ed1fc9c9e0644b87f9ef6a02c47d
Yeunwoong <60487315+Yeunwoong@users.noreply.github.com>
2020-04-02 08:27:11 +0900

    Add JDBC basic code

--- src/ex1/Program.java
+++ src/ex1/Program.java
@@ -1,12 +1,34 @@
 package ex1;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Connection con;
+		String jdbc_driver = "com.mysql.cj.jdbc.Driver";
+		String jdbc_url = "jdbc:mysql://localhost:3306/databasetest?serverTimezone=UTC";
+		try {
+			Class.forName(jdbc_driver).newInstance();
+			Connection con = DriverManager.getConnection(jdbc_url, "root", "123456");
+			Statement st = con.createStatement();
+			String sql = "SELECT * FROM databasetest.member";
+			ResultSet rs = st.executeQuery(sql);
+			
+			rs.next();
+			
+			@SuppressWarnings("unused")
+			String title = rs.getString("username");
+			System.out.printf("title: %s\n", title);
+			
+			rs.close();
+			st.close();
+			con.close();			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
 	}
 
 }
