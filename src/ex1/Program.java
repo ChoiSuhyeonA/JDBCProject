dc670c97cd7c8daa3cf1ff6212588458a45febd0
Yeunwoong <60487315+Yeunwoong@users.noreply.github.com>
2020-04-13 11:49:17 +0900

    with experiments

--- src/ex1/Program.java
+++ src/ex1/Program.java
@@ -14,8 +14,34 @@ public class Program {
 		try {
 			Class.forName(jdbc_driver).newInstance();
 			Connection con = DriverManager.getConnection(jdbc_url, "root", "123456");
+			Statement st = con.createStatement();
+			String sql = "SELECT * FROM databasetest.member";
+			ResultSet rs = st.executeQuery(sql);
 			
-			/*
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String userName = rs.getString("username");
+				String birth = rs.getString("birth");
+				String dept = rs.getString("dept");
+				String mail = rs.getString("email");
+				System.out.printf("id:  %d, userName: %s, birth: %s, dept: %s, email: %s"
+						+ "\n", id, userName, birth, dept, mail);
+			}		
+			
+			rs.close();
+			st.close();
+			con.close();
+			
+			/* 실습 2번
+			String sql = "INSERT INTO databasetest.member VALUES (7, 'Park', '1999-02-15', 'Art', 'park')";
+			st.executeUpdate(sql);
+			*/
+			
+			/* 실습 3번
+			String sql = "SELECT * FROM databasetest.member WHERE dept = 'computer'";
+			*/
+			
+			/* 실습 4번: PreparedStatement Example 
 			String stSql = "INSERT INTO databasetest.member VALUES (?, ?, ?, ?, ?)";
 			PreparedStatement st = con.prepareStatement(stSql);
 			st.setInt(1, 8);
@@ -24,32 +50,7 @@ public class Program {
 			st.setString(4, "Art");
 			st.setString(5, "oh");
 			st.executeUpdate();
-			*/
-			Statement st = con.createStatement();
-			
-			//String sql = "INSERT INTO databasetest.member VALUES (7, 'Park', '1999-02-15', 'Art', 'park')";
-			//st.executeUpdate(sql);
-			
-			//String sql = "SELECT * FROM databasetest.member WHERE dept = 'computer'";
-			String sql = "CREATE TABLE addressbook ("
-					+ "id INT NOT NULL, name VARCHAR(45) NOT NULL,"
-					+ "tel VARCHAR(45) NOT NULL, email VARCHAR(45) NOT NULL, address VARCHAR(45) NULL, PRIMARY KEY(id))"; 
-			st.executeUpdate(sql);
-			/*
-			while(rs.next()) {
-				@SuppressWarnings("unused")
-				int id = rs.getInt("id");
-				String userName = rs.getString("username");
-				String birth = rs.getString("birth");
-				String dept = rs.getString("dept");
-				String mail = rs.getString("email");
-				System.out.printf("id:  %d, userName: %s, birth: %s, dept: %s, email: %s"
-						+ "\n", id, userName, birth, dept, mail);
-			}*/
-			
-			//rs.close();
-			st.close();
-			con.close();			
+			*/		
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
